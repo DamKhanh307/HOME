d7289fc3c6290e6e9565018f2b13ee52994f1035
Đàm Khánh <ngockhanh2007yl@gmail.com>
2024-08-27 22:31:11 +0700

    Rebuild UI for IMG

--- IMG/js.js
+++ IMG/js.js
@@ -1,1 +1,72 @@
-function _0x2bea(_0x3d5341,_0x1be6fa){const _0x16380f=_0x1638();return _0x2bea=function(_0x2bea35,_0x206a0){_0x2bea35=_0x2bea35-0x158;let _0x1553e8=_0x16380f[_0x2bea35];return _0x1553e8;},_0x2bea(_0x3d5341,_0x1be6fa);}const _0x5e070c=_0x2bea;(function(_0x8e56f5,_0x1ef405){const _0x50f649=_0x2bea,_0x43f1c9=_0x8e56f5();while(!![]){try{const _0x41e604=parseInt(_0x50f649(0x15b))/0x1+parseInt(_0x50f649(0x163))/0x2+-parseInt(_0x50f649(0x16b))/0x3*(parseInt(_0x50f649(0x15c))/0x4)+parseInt(_0x50f649(0x164))/0x5+parseInt(_0x50f649(0x160))/0x6*(parseInt(_0x50f649(0x169))/0x7)+-parseInt(_0x50f649(0x176))/0x8*(-parseInt(_0x50f649(0x167))/0x9)+-parseInt(_0x50f649(0x177))/0xa;if(_0x41e604===_0x1ef405)break;else _0x43f1c9['push'](_0x43f1c9['shift']());}catch(_0x58f15f){_0x43f1c9['push'](_0x43f1c9['shift']());}}}(_0x1638,0xacfb3),document[_0x5e070c(0x16f)](_0x5e070c(0x165),function(){const _0x202242=_0x5e070c;var _0x17e690=document[_0x202242(0x15d)],_0x324074=screen['width'];console[_0x202242(0x161)](_0x17e690);_0x17e690[_0x202242(0x166)](_0x202242(0x171))||_0x17e690['includes']('http://127.0.0.1:5500')?console[_0x202242(0x161)]('Redirect\x20Done'):_0x324074<0x384?window[_0x202242(0x16d)]['href']=_0x202242(0x172):window[_0x202242(0x16d)][_0x202242(0x175)]=_0x202242(0x159);function _0x26e283(_0x486f9b){const _0x21ab32=_0x202242;fetch('https://api.github.com/repos/Vande541/Di/contents/Asset')[_0x21ab32(0x16a)](_0x1b3919=>{const _0x5e4b00=_0x21ab32;if(!_0x1b3919['ok'])throw new Error(_0x5e4b00(0x15e));return _0x1b3919[_0x5e4b00(0x16e)]();})[_0x21ab32(0x16a)](_0x5b5e79=>{globalData=_0x5b5e79,downloadUrls=_0x26348d(),_0x486f9b(downloadUrls);})[_0x21ab32(0x158)](_0xb337bc=>{console['error']('There\x20was\x20a\x20problem\x20with\x20the\x20fetch\x20operation:',_0xb337bc);});}function _0x26348d(){const _0x10774d=_0x202242;let _0x473455=globalData,_0x9ffe73=[];return _0x473455[_0x10774d(0x15f)](_0xea4259=>{const _0x1757d8=_0x10774d;_0x9ffe73[_0x1757d8(0x170)](_0xea4259['download_url']);}),_0x9ffe73;}function _0x319f8e(_0x1a8c40){const _0x317510=_0x202242;_0x1a8c40[_0x317510(0x15f)](_0x11cbad=>{const _0x13090c=_0x317510;let _0x1f305d=document[_0x13090c(0x15a)](_0x13090c(0x174));_0x1f305d[_0x13090c(0x16c)]=_0x11cbad;let _0x315338=document[_0x13090c(0x162)](_0x13090c(0x168));_0x315338[_0x13090c(0x173)](_0x1f305d);});}_0x26e283(function(_0x455862){_0x319f8e(_0x455862);});}));function _0x1638(){const _0x4c1d2b=['2312984enVOrP','6398265UbYAtK','DOMContentLoaded','includes','117gpeTGj','.illu','469alefyX','then','246642YlOmQW','src','location','json','addEventListener','push','https://damkhanh307.github.io/Home','https://damkhanh307.github.io/Home/404','appendChild','img','href','119656EclhMx','23783850KByGbN','catch','https://damkhanh307.github.io/Home/404-not-found/','createElement','465137IiYFsf','24OqKVop','referrer','Network\x20response\x20was\x20not\x20ok','forEach','43386rHWNdz','log','querySelector'];_0x1638=function(){return _0x4c1d2b;};return _0x1638();}
+document.addEventListener("DOMContentLoaded", function () {
+    var referrer = document.referrer;
+    var isLocalhost = window.location.hostname === "127.0.0.1" || window.location.hostname === "localhost";
+
+    if (isLocalhost || referrer.includes("https://damkhanh307.github.io/Home") || referrer.includes('http://127.0.0.1:5500')) {
+        console.log("Redirect Done");
+    } else if (screen.width < 900) {
+        window.location.href = "https://damkhanh307.github.io/Home/404";
+    } else {
+        window.location.href = "https://damkhanh307.github.io/Home/404-not-found/";
+    }
+
+    function fetchData(callback) {
+        fetch('https://api.github.com/repos/Vande541/Di/contents/Asset').then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.json();
+        }).then(data => {
+            globalData = data;
+            downloadUrls = getDownloadUrls();
+            callback(downloadUrls);
+        }).catch(error => {
+            console.error("There was a problem with the fetch operation:", error);
+        });
+    }
+
+    function getDownloadUrls() {
+        let urls = [];
+        globalData.forEach(item => {
+            urls.push(item.download_url);
+        });
+        return urls;
+    }
+
+    function displayImages(urls, callback) {
+        let loadedImages = 0;
+        let container = document.querySelector(".root");
+
+        urls.forEach(url => {
+            let anchorElement = document.createElement("a");
+            anchorElement.href = url;
+            anchorElement.setAttribute("data-fancybox", "gallery");
+            anchorElement.setAttribute("data-caption", `Image ${urls.indexOf(url) + 1}`);
+
+            let imgElement = document.createElement("img");
+            imgElement.src = url;
+            imgElement.onload = function () {
+                loadedImages++;
+                if (loadedImages === urls.length) {
+                    callback();
+                }
+            };
+            imgElement.onerror = function () {
+                console.log(`Image not found: ${imgElement.src}`);
+                imgElement.remove(); 
+            };
+
+            anchorElement.appendChild(imgElement);
+            container.appendChild(anchorElement);
+        });
+    }
+
+    fetchData(function (downloadUrls) {
+        displayImages(downloadUrls, checkAndRemoveBrokenImages);
+    });
+
+    function checkAndRemoveBrokenImages() {
+    }
+    Fancybox.bind("[data-fancybox]", {});
+    
+});
